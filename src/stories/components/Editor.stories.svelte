616a1d788c4c21f1121c429d5702edde849135f2
Dan Berry <deb1g19@soton.ac.uk>
2021-10-27 11:19:40 +0100

    Add story for editor
    Also added svelte typescript support to storybook.
    Ref deb1g19-individual-project/application#19

---
+++ src/stories/components/Editor.stories.svelte
@@ -0,0 +1,50 @@
+<script>
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import Editor from '../../components/editor/Editor.svelte';
+</script>
+
+<Meta
+	title="Editor/Text editor"
+	component={Editor}
+	argTypes={{
+		initialValue: { control: 'text' },
+		language: {
+			control: { type: 'select' },
+			options: ['jsx', 'html', 'css']
+		}
+	}}
+/>
+
+<Template let:args>
+	<Editor {...args} />
+</Template>
+
+<Story
+	name="JSX"
+	args={{
+		initialValue: `export const test = () => {
+  console.log("Hello, world!")
+}`,
+		language: 'jsx'
+	}}
+/>
+
+<Story
+	name="HTML"
+	args={{
+		initialValue: `<div>
+  <p>Hello, world!</p>
+</div>`,
+		language: 'html'
+	}}
+/>
+
+<Story
+	name="CSS"
+	args={{
+		initialValue: `.some-class {
+  background-color: 'red';
+}`,
+		language: 'css'
+	}}
+/>
